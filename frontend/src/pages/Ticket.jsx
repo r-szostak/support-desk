6fac11c5731532e399db2df349c05ec289334da4
Rafal Szostak <rafal.szostak2@gmail.com>
2022-09-29 16:26:15 +0200

    Added Frontend notes

---
+++ frontend/src/pages/Ticket.jsx
@@ -0,0 +1,77 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getTicket, closeTicket } from "../features/tickets/ticketSlice"
+import { getNotes, reset as notesReset } from "../features/notes/noteSlice"
+import { useParams, useNavigate } from "react-router-dom"
+import Spinner from "../components/Spinner"
+import BackButton from "../components/BackButton"
+import { toast } from "react-toastify"
+import NoteItem from "../components/NoteItem"
+import Modal from "react-modal"
+
+function Ticket() {
+  const { ticket, isLoading, isError, message } = useSelector(
+    (state) => state.ticket
+  )
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes
+  )
+
+  const dispatch = useDispatch()
+  const { ticketId } = useParams()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    dispatch(getTicket(ticketId))
+    dispatch(getNotes(ticketId))
+  }, [dispatch, ticketId, isError, message])
+
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId))
+    toast.success("Ticket Closed")
+    navigate("/tickets")
+  }
+
+  if (isLoading || notesIsLoading) return <Spinner />
+
+  if (isError) return <h3>Something Went Wrong</h3>
+
+  return (
+    <div className="ticket-page">
+      <header className="ticket-header">
+        <BackButton url="/tickets" />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString("pl-PL")}
+        </h3>
+        <h3>Product: {ticket.product}</h3>
+        <hr />
+        <div className="ticket-desc">
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+        <h2>Notes</h2>
+      </header>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
+
+      {ticket.status !== "closed" && (
+        <button className="btn btn-block btn-danger" onClick={onTicketClose}>
+          Close Ticket
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default Ticket
