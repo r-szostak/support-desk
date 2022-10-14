d4ec480cd9aea671d82af37908350c278a76cfc4
Rafal Szostak <rafal.szostak2@gmail.com>
2022-10-14 15:35:20 +0200

    Heroku

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,6 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
-      useCreateIndex: true,
       useUnifiedTopology: true,
     })
     console.log(`MongoFB Connected: ${conn.connection.host}`.cyan.underline)
