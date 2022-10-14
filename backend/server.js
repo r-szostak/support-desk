0ca5d65991a81985134733f6f1880f83e31b8752
Rafal Szostak <rafal.szostak2@gmail.com>
2022-10-14 14:04:24 +0200

    Server.js modified

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,12 @@ app.use("/api/tickets", require("./routes/ticketRoutes"))
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")))
 
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
-  )
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+  })
 } else {
-  app.get("/", (req, res) => {
-    res.status(200).json({ message: "Welcome to the Support desk API" })
+  app.get("/", (_, res) => {
+    res.status(200).json({ message: "Welcome to the Support Desk API" })
   })
 }
 
