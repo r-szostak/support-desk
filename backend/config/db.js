6eb5996e3463cefc65f7d195a91dbe4c70748a08
Rafal Szostak <rafal.szostak2@gmail.com>
2022-10-14 14:49:11 +0200

    Heroku

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL)
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
     console.log(`MongoFB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
     console.log(`Error: ${error.message}`.red.underline.bold)
