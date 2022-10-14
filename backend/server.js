f9f6f2877fc0af7258e4a79f7a087ef05938008e
Rafal Szostak <rafal.szostak2@gmail.com>
2022-10-14 14:24:15 +0200

    Heroku debuging

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const express = require("express")
-const colors = require("colors")
-const dotenv = require("dotenv").config()
+require("colors")
+require("dotenv").config()
 const { errorHandler } = require("./middleware/errorMiddleware")
 const connectDB = require("./config/db")
 const PORT = process.env.PORT || 5000
