e1cc3be9dcfccd0b49033da3357b738ece5d1b02
Rafal Szostak <rafal.szostak2@gmail.com>
2023-04-05 14:48:46 +0200

    Server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(express.urlencoded({ extended: false }))
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"))
