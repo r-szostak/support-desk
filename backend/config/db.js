5ef4d66f1ebbe355b56f68ddd1bb929111e14d74
Rafal Szostak <rafal.szostak2@gmail.com>
2023-04-05 14:23:56 +0200

    Try Catch improvment in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose")
 
 const connectDB = async () => {
-  mongoose.set("strictQuery", true)
-  mongoose.connect(process.env.MONGO_URL).then(
-    console.log("Conntected to MongoDB").catch((err) => {
-      console.error("Failed to connect with MongoDB")
-      console.error(err)
-    })
-  )
+  try {
+    mongoose.set("strictQuery", true)
+    mongoose.connect(process.env.MONGO_URL)
+    console.log("Conntected to MongoDB")
+  } catch (err) {
+    console.error("Failed to connect with MongoDB")
+    console.error(err)
+  }
 }
 
 module.exports = connectDB
